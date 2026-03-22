9912a3f401d5b27b87309798750affd60a6372dc
Josh Goodwin <j@joshuagoodw.in>
2026-03-22 12:30:18 +0000

    randomise the order of querysets to detect nondeterminism

--- buses/settings.py
+++ buses/settings.py
@@ -16,6 +16,7 @@ CSRF_TRUSTED_ORIGINS = os.environ.get(
 ).split()
 
 TEST = "test" in sys.argv or "pytest" in sys.argv[0]
+TEST_RUNNER = "buses.test_runner.RandomOrderQuerySetRunner"
 DEBUG = bool(os.environ.get("DEBUG"))
 
 DEFAULT_FROM_EMAIL = '"bustimes.org" <bustimes.org@bustimes.org>'
